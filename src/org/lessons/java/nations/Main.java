2aeda2dbda1b41baa6b3d0751d0817e8fbaf3c7d
Alastor2189 <g.lucafarci89@gmail.com>
2023-02-08 16:50:45 +0100

    Avevo dimenticato la ricerca avanzata

--- src/org/lessons/java/nations/Main.java
+++ src/org/lessons/java/nations/Main.java
@@ -1,25 +1,26 @@
 package org.lessons.java.nations;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) throws SQLException {
+		
+		
 		String url = "jdbc:mysql://localhost:3306/Db_Nations";
 		String user = "root";
 		String password = "root";
+		
+		
 		Scanner s = new Scanner(System.in);
-		System.out.print("Cosa vuoi cercare?");
-		String cercaNazione = s.nextLine();
-		s.close();
+		
+		
+		
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
-
+			System.out.print("Cosa vuoi cercare?");
+			String cercaNazione = s.nextLine();
+			String filtroAvanzato = "%" + cercaNazione + "%";
+			
 			String sql = "SELECT countries.country_id  as id_paese ,countries.name as nome_paese, "
-					+ "regions.name as nome_regione, " + "continents.name as nome_continente\n" + "FROM countries \n"
+					+ "regions.name as nome_regione, " + "continents.name as nome_continente\n" 
+					+ "FROM countries \n"
 					+ "Inner join regions \n" + "on countries.region_id  = regions.region_id  \n"
 					+ "Inner join continents \n" + "on regions.continent_id = continents.continent_id \n"
 					+"WHERE countries.name LIKE ?"
@@ -43,5 +44,8 @@ public class Main {
 
 			ex.printStackTrace();
 		}
+		s.close();
+
 	}
 }
+
